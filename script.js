b540c13ef8063ef61e484ae51db4be3710a832ae
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2021-08-19 21:05:52 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputArea = document.querySelector("#place-text");
+const outputArea = document.querySelector("#language");
+const translateBtn = document.querySelector("#translate-btn");
+
+var minionsURL = "https://api.funtranslations.com/translate/doge.json";
+
+function translateToMinion(text){
+    return minionsURL + "?" + "text=" + text;
+};
+
+function errorHandler(error){
+    console.log("error occured : ", error);
+};
+
+function clickHandler(){
+    var textInput = inputArea.value;
+
+    fetch(translateToMinion(textInput))
+    .then(response => response.json())
+    .then(json => showOutput(json.contents.translated))
+    .catch(errorHandler)
+    document.querySelector('#language').style.cssText = `color: black;`;
+};
+
+function showOutput(text){
+    outputArea.innerHTML = text;
+}
+
+translateBtn.addEventListener("click", clickHandler);
